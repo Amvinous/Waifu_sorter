4fbc3dbcc6454d93d30af4ab5e97b371e4efc991
Ambi <necrogamersltu@gmail.com>
2023-06-24 13:00:27 +0200

    Started making UI

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-## * imports
+# * imports
 import requests as rq
 import json
 import pathlib
 import os
 from fuzzywuzzy import fuzz
 from random import sample
+from PIL import Image
+from io import BytesIO
+# * pygame
+import pygame
 
 
 class Character:
@@ -116,7 +120,7 @@ class Sorter:
         left_index, right_index = 0, 0
         result = []
 
-        while (len(result) < len(left) + len(right)):
+        while len(result) < len(left) + len(right):
             print(f'{left[left_index]} or {right[right_index]}')
             user_input = input("0 or 1")
             if user_input == 'exit':
@@ -135,11 +139,54 @@ class Sorter:
 
 if __name__ == '__main__':
     # * Inputs
-    user_input = input("Enter Account name: ").strip()
+    # user_input = input("Enter Account name: ").strip()
     path = pathlib.Path("Users")
     user_list = list(filter(lambda p: p.is_file(), path.glob("*.txt")))
-    Sorting = Sorter(user_input, path, user_list)
+    Sort = Sorter("Amvi", path, user_list)
+    # Sort = Sorter(user_input, path, user_list)
     # * Lists
-    username, char_list = Sorting.fetch(Sorting.user_input)
+    username, char_list = Sort.fetch(Sort.user_input)
     object_dict = {char[0]: Character(char[0], char[1], char[2]) for char in char_list}
-    sorted_list = list(Sorting.merge_sort(sample([char[0] for char in char_list], len([char[0] for char in char_list]))))
+
+# ? pygame
+
+# ? Configure window
+
+display = pygame.display
+background_color = (30, 30, 30)
+screen = display.set_mode((1280, 720))
+display.set_caption("Anilist Sorter")
+
+# ? add icon
+
+icon_image = Image.open(BytesIO(rq.get("https://i.imgur.com/9iPDCuY.png").content))
+icon_image_rgba = icon_image.convert("RGBA")
+image_data_rgba = icon_image_rgba.tobytes()
+image_data_size = icon_image_rgba.size
+new_icon = pygame.image.fromstring(image_data_rgba, icon_image.size, "RGBA")
+
+# ? add images
+
+image1 = pygame.image.load(BytesIO(rq.get(object_dict["Miku Nakano"].pic).content))
+image2 = pygame.image.load(BytesIO(rq.get(object_dict["Kana Arima"].pic).content))
+
+display.set_icon(new_icon)
+
+# ? loop
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+
+    screen.fill(background_color)
+
+    screen.blit(image1, (50, 50))
+    screen.blit(image2, (200, 50))
+    pygame.display.update()
+
+pygame.quit()
