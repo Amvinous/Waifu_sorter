8441005dbdddb49faf84f1deb0f5cd1ca953183c
Ambi <necrogamersltu@gmail.com>
2023-07-02 19:19:39 +0200

    -Added functionality to buttons and sorting -Made it download images while initially getting the list -replaced object_dict[].pic in Sorter from storing urls to image objects

--- Sorter.py
+++ Sorter.py
@@ -1,10 +1,17 @@
-import requests as rq
 import json
 import os
 from fuzzywuzzy import fuzz
-from random import sample
-import pathlib
+
 from Character import Character
+
+# * imports
+import requests as rq
+import pathlib
+# * Image
+from PIL import Image
+from io import BytesIO
+# * pygame
+import pygame
 
 
 class MergeExitException(Exception):
@@ -17,16 +24,30 @@ class PersonNotFound(Exception):
 
 class Sorter:
     def __init__(self, user_input, path, user_list):
-        # * Retrieve information
+        self.clock = None
+        self.button1 = None
+        self.button2 = None
+        self.screen = None
+        self.object_dict = None
+        self.name_list = None
+        self.char_list = []
+        self.user = None
         self.user_input = user_input
-        self.user = ""
         self.path = path
         self.user_list = user_list
-        self.object_dict = []
+        self.user_path = pathlib.Path()
 
     # * Gets list from memory
+    def download_image(self, file_name, url, path):
+        response = rq.get(url)
+        if response.status_code == 200:
+            with open(path / f'{file_name}.png', "wb") as file:
+                picture = file.write(response.content)
+        else:
+            print("Error with retrieving images")
+        return picture
+
     def get_list(self):
-        # ?returns 2 variables
         query = """
     query ($id: String, $page: Int){
                 User(search: $id){
@@ -57,8 +78,6 @@ class Sorter:
         }
         url = 'https://graphql.anilist.co'
 
-        char_list = []
-
         while True:
             response = rq.post(url, json={'query': query, 'variables': variables}).json()
 
@@ -73,7 +92,7 @@ class Sorter:
                 gender = character["gender"]
 
                 # ? add more attributes to the dictionary here and in class
-                char_list.append((name, pic, gender))
+                self.char_list.append([name, pic, gender])
 
             if not data["pageInfo"]["hasNextPage"]:
                 break
@@ -81,10 +100,14 @@ class Sorter:
                 variables["page"] += 1
 
         user = response["data"]["User"]["name"]
-        new_user = open(self.path / f'{user}.txt', "w")
+        self.user_path = self.path / f"{user}"
+        self.user_path.mkdir(parents=True, exist_ok=True)
+        new_user = open(self.user_path / f'{user}.txt', "w")
         self.user = user
-        new_user.write(json.dumps(char_list, indent=2))
-        return char_list
+        new_user.write(json.dumps(self.char_list, indent=2))
+        print("Downloading images")
+        for character in self.char_list:
+            self.download_image(character[0], character[1], self.user_path)
 
     # * Searches memory
 
@@ -95,57 +118,87 @@ class Sorter:
             if comparison > 80:
                 self.user = username
                 user_in_memory = open(user)
-                char_list = json.load(user_in_memory)
+                self.char_list = json.load(user_in_memory)
                 break
         else:
             print("User not in memory  \n Searching Anilist")
-            char_list = self.get_list()
+            self.get_list()
             print("Found")
-        self.object_dict = {char[0]: Character(char[0], char[1], char[2]) for char in char_list}
-        return char_list
 
-# * Sorting
-    def merge_sort(self, array):
-        if len(array) < 2:
-            return array
-        mid = len(array) // 2
-        left = self.merge_sort(array[:mid])
-        right = self.merge_sort(array[mid:])
+        for char in self.char_list:
+            image = Image.open(self.path/self.user/f'{char[0]}.png')
+            image_data = image.copy()
+            char[1] = image_data
+        self.object_dict = {char[0]: Character(char[0], char[1], char[2]) for char in self.char_list}
+        self.name_list = [char[0] for char in self.char_list]
 
-        if left is None or right is None:
-            return None
-        return self.merge(left, right)
+    # * Sorting
 
-    def merge(self, left, right):
-        if not len(left) or not len(right):
-            return left or right
-        left_index, right_index = 0, 0
-        result = []
+    def merge_sort(self, char_list):
 
-        while len(result) < len(left) + len(right):
-            print(f'{left[left_index]} or {right[right_index]}')
-            user_input = input("0 or 1")
-            if user_input == 'exit':
-                raise MergeExitException
-            elif int(user_input) == 0:
-                result.append(left[left_index])
+        if len(char_list) > 1:
+            mid = len(char_list) // 2
+            left = char_list[:mid]
+            right = char_list[mid:]
+
+            self.merge_sort(left)
+            self.merge_sort(right)
+            k = 0
+            left_index = 0
+            right_index = 0
+            while left_index < len(left) and right_index < len(right):
+                print(left[left_index], right[right_index])
+                self.clock.tick(5)
+                self.button1.is_clicked = False
+                self.button2.is_clicked = False
+                for event in pygame.event.get():
+
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        exit()
+
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        self.button1.click(event.pos)
+                        self.button2.click(event.pos)
+
+                self.button1.draw(self.screen)
+                self.button2.draw(self.screen)
+                pygame.display.flip()
+
+                if self.button1.is_clicked:
+                    char_list[k] = left[left_index]
+                    left_index += 1
+                    k += 1
+                elif self.button2.is_clicked:
+                    char_list[k] = right[right_index]
+                    right_index += 1
+                    k += 1
+            while left_index < len(left):
+                char_list[k] = left[left_index]
                 left_index += 1
-            elif int(user_input) == 1:
-                result.append(right[right_index])
+                k += 1
+
+            while right_index < len(right):
+                char_list[k] = right[right_index]
                 right_index += 1
-            if left_index == len(left) or right_index == len(right):
-                result.extend(left[left_index:] or right[right_index:])
-                break
-        return result
+                k += 1
+            return char_list
+
+    def sorting(self, clock, button1, button2, screen):
+        self.button1 = button1
+        self.screen = screen
+        self.button2 = button2
+        self.clock = clock
+        self.name_list = self.merge_sort(self.name_list)
 
 
-
-
+"""
 if __name__ == '__main__':
     # * Inputs
     user_input = input("Enter Account name: ").strip()
     user_path = pathlib.Path("Users")
-    user_list = list(filter(lambda p: p.is_file(), user_path.glob("*.txt")))
+    user_list = list(filter(lambda p: p.is_file(), user_path.rglob("**/*.txt")))
     sort = Sorter(user_input, user_path, user_list)
     # * Lists
-
+    sort.fetch()
+"""
--- main.py
+++ main.py
@@ -11,6 +11,19 @@ import pygame
 from Sorter import Sorter
 
 
+class Button:
+    def __init__(self, x, y, width, height, color):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = color
+        self.is_clicked = False
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect)
+
+    def click(self, pos):
+        self.is_clicked = self.rect.collidepoint(pos)
+
+
 class RunWindow:
 
     def __init__(self):
@@ -22,35 +35,34 @@ class RunWindow:
         self.screen = None
         self.text_color = (250, 250, 250)
         self.clock = pygame.time.Clock()
-        self.running = True
         self.image1 = None
         self.image2 = None
+        self.running = True
 
     def init_window(self):
-        self.screen = self.display.set_mode((1280, 720))
+        # * create the actual window
+        width, height = 400, 600
+        self.screen = self.display.set_mode((width, height))
         self.display.set_caption("Anilist Sorter")
         self.screen.fill(self.background_color)
-
-        self.make_label(50, 50, 200, 100, sort.user)
-        self.add_images()
+        # * add different aspect to the window
+        self.make_label(50, 25, width * 0.8, height / 8, sort.user)
+        # self.add_images()
         self.add_icon()
-
+        # * initialize
         self.initialized = True
 
     def add_icon(self):
-        icon_image = Image.open(BytesIO(rq.get("https://i.imgur.com/9iPDCuY.png").content))
-        icon_image_rgba = icon_image.convert("RGBA")
-        image_data_rgba = icon_image_rgba.tobytes()
+        icon_image_rgba = Image.open(BytesIO(rq.get("https://i.imgur.com/9iPDCuY.png").content)).convert("RGBA")
+        image_data_bytes = icon_image_rgba.tobytes()
         image_data_size = icon_image_rgba.size
-        new_icon = pygame.image.frombytes(image_data_rgba, image_data_size, "RGBA")
+        new_icon = pygame.image.frombytes(image_data_bytes, image_data_size, "RGBA")
         self.display.set_icon(new_icon)
 
     def add_images(self):
-        dict_iter = iter(sort.object_dict.values())
 
-        self.image1 = pygame.image.load(BytesIO(rq.get(next(dict_iter).pic).content)).convert()
-        self.image2 = pygame.image.load(BytesIO(rq.get(next(dict_iter).pic).content)).convert()
-
+        self.image1 = pygame.image.load(BytesIO(sort.object_dict["left"].pic)).convert()
+        self.image2 = pygame.image.load(BytesIO(sort.object_dict["right"].pic)).convert()
 
     def make_label(self, x, y, width, height, label):
         pygame.draw.rect(self.screen, (0, 0, 0), (x, y, width, height))
@@ -62,19 +74,21 @@ class RunWindow:
     def main_loop(self):
 
         self.init_window()
-
+        button1 = Button(50, 350, 100, 50, (222, 136, 24))
+        button2 = Button(200, 350, 100, 50, (222, 136, 24))
+        sort.sorting(self.clock, button1, button2, self.screen)
+        print(sort.name_list)
         while self.running:
-            self.clock.tick(60)
+            self.clock.tick(5)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-
             if self.initialized:
-
-
                 self.screen.blit(self.image1, (50, 150))
                 self.screen.blit(self.image2, (200, 150))
-                self.display.flip()
+                button1.draw(self.screen)
+                button2.draw(self.screen)
+            self.display.flip()
 
         pygame.quit()
 
@@ -83,20 +97,16 @@ if __name__ == '__main__':
     # * Inputs
     user_input = input("Enter Account name: ").strip()
     path = pathlib.Path("Users")
-    user_list = list(filter(lambda p: p.is_file(), path.glob("*.txt")))
-    #! Automatic input
+    user_list = list(filter(lambda p: p.is_file(), path.rglob("**/*.txt")))
+    # ! Automatic input
     # sort = Sorter("Amvi", path, user_list)
-    #! Manual input
+    # ! Manual input
     sort = Sorter(user_input, path, user_list)
 
     # * Lists
-    char_list = sort.fetch()
-    name_list = [char[0] for char in char_list]
-    random_list = sample(name_list, len(name_list))
-    # sorted_list = list(
-    #    Sort.merge_sort(sample([char[0] for char in char_list], len([char[0] for char in char_list]))))
+    sort.fetch()
 
-    #* Run Window
+    # * Run Window
 
     window = RunWindow()
     window.main_loop()
